ec142f3dddf659114f28a61189dbd241fde9e7a6
unknown <2135142@labinfo.is.escuelaing.edu.co>
2019-03-18 13:37:11 -0500

    api

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,9 +1,19 @@
 
 var ControllerModule =(function(){
 	
-	CinemaByname =function(name){	       
-	       console.log(name); 
-	       
+	CinemaByname =function(cine){
+		var tabla= $('table');
+		var html;
+		if(cine!=null){
+			var funciones=cine[0].functions;
+			console.log(funciones);
+		}		
+	    console.log(cine);
+	    html = '<tr>';
+	    html+=("<td>"+cine[0].name+"<td>");
+	    html+=("<td>"+cine[0].functions.length+"<td>");
+	    html+='</tr>';
+	    tabla.append(html)
 	};
 	
 
