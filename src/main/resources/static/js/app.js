5ec6e01f4a62d9fc73a0f039680308618659bebe
Italo Novoa <italolo97@hotmail.com>
2019-03-20 13:53:21 -0500

    getCinemaByName

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,19 +1,20 @@
 
-var ControllerModule =(function(){
+var app =(function(){
 	
 	CinemaByname =function(cine){
-		var tabla= $('table');
+		var tabla= $('#Cine');
 		var html;
-		if(cine!=null){
+		tabla.empty();
+		if(cine!=null){			
 			var funciones=cine[0].functions;
-			console.log(funciones);
+			html =' <tr style="border: 1px solid black"> <th colspan="2">Cinema</th> </tr>	<tr style="border: 1px solid black">'
+			html+='<th>Name</th>	<th>NumFunciones</th> </tr>'
+			html+='<tr>'+"\n	<td>"+cine[0].name+"</td>"+"\n	<td>"+cine[0].functions.length+"</td>"+'\n</tr>';		    
 		}		
-	    console.log(cine);
-	    html = '<tr>';
-	    html+=("<td>"+cine[0].name+"<td>");
-	    html+=("<td>"+cine[0].functions.length+"<td>");
-	    html+='</tr>';
-	    tabla.append(html)
+	  
+		 tabla.append(html);
+	    
+	    
 	};
 	
 
